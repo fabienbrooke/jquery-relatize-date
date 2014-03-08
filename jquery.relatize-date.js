070da1b4c9712e9601e59be82bc557bf23dce461
Fabien <fabienbrooke@gmail.com>
2014-03-08 11:58:55 -0800

    Clean up
    remove implicit global, rename file to swap underscore for hyphen, add a few semicolons for consistency and fix jsdoc

--- jquery.relatize-date.js
+++ jquery.relatize-date.js
@@ -2,16 +2,16 @@
 (function($) {
   $.fn.relatizeDate = function() {
     return $(this).each(function() {
-      $(this).text( $.relatizeDate(this) )
+      $(this).text( $.relatizeDate(this) );
     })
-  }
+  };
 
   $.relatizeDate = function(element) {
-    return $.relatizeDate.timeAgoInWords( new Date($(element).text()) )
-  }
+    return $.relatizeDate.timeAgoInWords( new Date($(element).text()) );
+  };
 
   // shortcut
-  $r = $.relatizeDate
+  var $r = $.relatizeDate;
 
   $.extend($.relatizeDate, {
     shortDays: [ 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' ],
@@ -22,71 +22,72 @@
     /**
      * Given a formatted string, replace the necessary items and return.
      * Example: Time.now().strftime("%B %d, %Y") => February 11, 2008
+     * @param {Date} date The date to use in the formatting
      * @param {String} format The formatted string used to format the results
      */
     strftime: function(date, format) {
       var day = date.getDay(), month = date.getMonth();
       var hours = date.getHours(), minutes = date.getMinutes();
 
-      var pad = function(num) { 
+      var pad = function(num) {
         var string = num.toString(10);
         return new Array((2 - string.length) + 1).join('0') + string
       };
 
-      return format.replace(/\%([aAbBcdHImMpSwyY])/g, function(part) {
+      return format.replace(/%([aAbBcdHImMpSwyY])/g, function(part) {
         switch(part[1]) {
-          case 'a': return $r.shortDays[day]; break;
-          case 'A': return $r.days[day]; break;
-          case 'b': return $r.shortMonths[month]; break;
-          case 'B': return $r.months[month]; break;
-          case 'c': return date.toString(); break;
-          case 'd': return pad(date.getDate()); break;
-          case 'H': return pad(hours); break;
-          case 'I': return pad((hours + 12) % 12); break;
-          case 'm': return pad(month + 1); break;
-          case 'M': return pad(minutes); break;
-          case 'p': return hours > 12 ? 'PM' : 'AM'; break;
-          case 'S': return pad(date.getSeconds()); break;
-          case 'w': return day; break;
-          case 'y': return pad(date.getFullYear() % 100); break;
-          case 'Y': return date.getFullYear().toString(); break;
+          case 'a': return $r.shortDays[day];
+          case 'A': return $r.days[day];
+          case 'b': return $r.shortMonths[month];
+          case 'B': return $r.months[month];
+          case 'c': return date.toString();
+          case 'd': return pad(date.getDate());
+          case 'H': return pad(hours);
+          case 'I': return pad((hours + 12) % 12);
+          case 'm': return pad(month + 1);
+          case 'M': return pad(minutes);
+          case 'p': return hours > 12 ? 'PM' : 'AM';
+          case 'S': return pad(date.getSeconds());
+          case 'w': return day;
+          case 'y': return pad(date.getFullYear() % 100);
+          case 'Y': return date.getFullYear().toString();
         }
-      })
+      });
     },
-  
+
     timeAgoInWords: function(targetDate, includeTime) {
       return $r.distanceOfTimeInWords(targetDate, new Date(), includeTime);
     },
-  
+
     /**
      * Return the distance of time in words between two Date's
      * Example: '5 days ago', 'about an hour ago'
      * @param {Date} fromTime The start date to use in the calculation
      * @param {Date} toTime The end date to use in the calculation
-     * @param {Boolean} Include the time in the output
+     * @param {Boolean} includeTime the time in the output
      */
     distanceOfTimeInWords: function(fromTime, toTime, includeTime) {
       var delta = parseInt((toTime.getTime() - fromTime.getTime()) / 1000);
       if (delta < 60) {
-          return 'less than a minute ago';
+        return 'less than a minute ago';
       } else if (delta < 120) {
-          return 'about a minute ago';
+        return 'about a minute ago';
       } else if (delta < (45*60)) {
-          return (parseInt(delta / 60)).toString() + ' minutes ago';
+        return (parseInt(delta / 60)).toString() + ' minutes ago';
       } else if (delta < (120*60)) {
-          return 'about an hour ago';
+        return 'about an hour ago';
       } else if (delta < (24*60*60)) {
-          return 'about ' + (parseInt(delta / 3600)).toString() + ' hours ago';
+        return 'about ' + (parseInt(delta / 3600)).toString() + ' hours ago';
       } else if (delta < (48*60*60)) {
-          return '1 day ago';
+        return '1 day ago';
       } else {
         var days = (parseInt(delta / 86400)).toString();
         if (days > 5) {
-          var fmt  = '%B %d, %Y'
-          if (includeTime) fmt += ' %I:%M %p'
+          var fmt  = '%B %d, %Y';
+          if (includeTime) fmt += ' %I:%M %p';
           return $r.strftime(fromTime, fmt);
         } else {
-          return days + " days ago"
+          return days + " days ago";
         }
       }
     }
